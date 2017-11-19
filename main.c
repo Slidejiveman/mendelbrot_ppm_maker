60a86451001e78d9d7012e27ea2a33cbc1303111
Slidejiveman <rdnotlaw91@outlook.com>
2017-11-19 15:48:00 -0600

    Found a better example to play with.

--- main.c
+++ main.c
@@ -1,96 +1,150 @@
-/*
- c program:
- --------------------------------
-  1. draws Mandelbrot set for Fc(z)=z*z +c
-  using Mandelbrot algorithm ( boolean escape time )
- -------------------------------
- 2. technique of creating ppm file is  based on the code of Claudio Rocchini
- http://en.wikipedia.org/wiki/Image:Color_complex_plot.jpg
- create 24 bit color graphic file ,  portable pixmap file = PPM
- see http://en.wikipedia.org/wiki/Portable_pixmap
- to see the file use external application ( graphic viewer)
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <omp.h>
 
- This code was taken from Rosetta Code so that I could learn
- about how the Mendelbrot set works, etc.
+void Gradient(double position,unsigned char c[3]);
 
- I open the PPM files using Gimp.
- Reference: https://rosettacode.org/wiki/Mandelbrot_set#C
-  */
- #include <stdio.h>
- #include <math.h>
- int main()
- {
-          /* screen ( integer) coordinate */
-        int iX,iY;
-        const int iXmax = 800;
-        const int iYmax = 800;
-        /* world ( double) coordinate = parameter plane*/
-        double Cx,Cy;
-        const double CxMin=-2.5;
-        const double CxMax=1.5;
-        const double CyMin=-2.0;
-        const double CyMax=2.0;
-        /* */
-        double PixelWidth=(CxMax-CxMin)/iXmax;
-        double PixelHeight=(CyMax-CyMin)/iYmax;
-        /* color component ( R or G or B) is coded from 0 to 255 */
-        /* it is 24 bit color RGB file */
-        const int MaxColorComponentValue=255;
-        FILE * fp;
-        char *filename="new1.ppm";
-        char *comment="# ";/* comment should start with # */
-        static unsigned char color[3];
-        /* Z=Zx+Zy*i  ;   Z0 = 0 */
-        double Zx, Zy;
-        double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
-        /*  */
-        int Iteration;
-        const int IterationMax=200;
-        /* bail-out value , radius of circle ;  */
-        const double EscapeRadius=2;
-        double ER2=EscapeRadius*EscapeRadius;
-        /*create new file,give it a name and open it in binary mode  */
-        fp= fopen(filename,"wb"); /* b -  binary mode */
-        /*write ASCII header to the file*/
-        fprintf(fp,"P6\n %s\n %d\n %d\n %d\n",comment,iXmax,iYmax,MaxColorComponentValue);
-        /* compute and write image data bytes to the file*/
-        for(iY=0;iY<iYmax;iY++)
-        {
-             Cy=CyMin + iY*PixelHeight;
-             if (fabs(Cy)< PixelHeight/2) Cy=0.0; /* Main antenna */
-             for(iX=0;iX<iXmax;iX++)
-             {
-                        Cx=CxMin + iX*PixelWidth;
-                        /* initial value of orbit = critical point Z= 0 */
-                        Zx=0.0;
-                        Zy=0.0;
-                        Zx2=Zx*Zx;
-                        Zy2=Zy*Zy;
-                        /* */
-                        for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
-                        {
-                            Zy=2*Zx*Zy + Cy;
-                            Zx=Zx2-Zy2 +Cx;
-                            Zx2=Zx*Zx;
-                            Zy2=Zy*Zy;
-                        };
-                        /* compute  pixel color (24 bit = 3 bytes) */
-                        if (Iteration==IterationMax)
-                        { /*  interior of Mandelbrot set = black */
-                           color[0]=0;
-                           color[1]=0;
-                           color[2]=0;
-                        }
-                     else
-                        { /* exterior of Mandelbrot set = white */
-                             color[0]=255; /* Red*/
-                             color[1]=255;  /* Green */
-                             color[2]=255;/* Blue */
-                        };
-                        /*write color to the file*/
-                        fwrite(color,1,3,fp);
+// picture resolution
+static const int ImageWidth  = 1000;
+static const int ImageHeight = 1000;
+static const double CxMin    = -2.5;
+static const double CxMax    = 1.5;
+static const double CyMin    = -2.0;
+static const double CyMax    = 2.0;
+
+int main()
+{
+    double PixelWidth=(CxMax-CxMin)/ImageWidth; /* scaled x coordinate of pixel (must be scaled to lie somewhere in the Mandelbrot X scale (-2.5, 1.5) */
+    double PixelHeight=(CyMax-CyMin)/ImageHeight;/* scaled y coordinate of pixel (must be scaled to lie somewhere in the Mandelbrot Y scale (-2.0, 2.0) */
+
+    const int MaxColorComponentValue=255;
+    typedef unsigned char pixel_t[3]; // colors [R, G ,B]
+    pixel_t *pixels = malloc(sizeof(pixel_t)*ImageHeight*ImageWidth);
+
+
+    FILE* fp;
+
+#pragma omp parallel shared(pixels)
+    {
+        int iY;
+#pragma omp for schedule(dynamic)
+        for(iY=0; iY<ImageHeight; iY++) {
+            double Cy=CyMin + iY*PixelHeight;
+            if (fabs(Cy)< PixelHeight/2) {
+                Cy=0.0; // Main antenna
+            }
+            int iX;
+            for(iX=0; iX<ImageWidth; iX++) {
+                double Cx=CxMin + iX*PixelWidth;
+                double Zx=0.0;
+                double Zy=0.0;
+                double Zx2=Zx*Zx;
+                double Zy2=Zy*Zy;
+                /* */
+                int Iteration;
+                const int IterationMax=50;
+                const double Bailout=2; // bail-out value
+                const double Circle_Radius=Bailout*Bailout; // circle radius
+
+                for (Iteration=0; Iteration<IterationMax && ((Zx2+Zy2)<Circle_Radius); Iteration++) { // 
+                    Zy=2*Zx*Zy + Cy;
+                    Zx=Zx2-Zy2 + Cx;
+                    Zx2=Zx*Zx;
+                    Zy2=Zy*Zy;
+                };
+
+                if (Iteration==IterationMax) {
+                    //  interior of Mandelbrot set = black
+                    pixels[iY*ImageWidth + iX][0] = 0;
+                    pixels[iY*ImageWidth + iX][1] = 0;
+                    pixels[iY*ImageWidth + iX][2] = 0;
                 }
+                    //
+                else {
+                    Gradient((double)((Iteration-log2(log2(sqrt(Zx2+Zy2)))))/IterationMax,pixels[iY*ImageWidth + iX]);
+                }
+            }
         }
-        fclose(fp);
-        return 0;
- }
+    }
+
+    fp= fopen("MandelbrotSetNEW.ppm","wb");
+    fprintf(fp,"P6\n %s\n %d\n %d\n %d\n","# no comment",ImageWidth,ImageHeight,MaxColorComponentValue);
+    fwrite(pixels,sizeof(pixel_t),ImageWidth*ImageWidth,fp);
+    fclose(fp);
+    free(pixels);
+
+    return 0;
+}
+
+void Gradient(double position,unsigned char c[3])
+{
+
+
+    if (position>1.0){
+        if (position-(int)position==0.0) position=1.0;
+
+        else position=position-(int)position;
+    }
+
+
+
+
+    unsigned char nmax=7; // number of color bars
+    double m=nmax*position;
+
+    int n=(int)m;
+
+    double f=m-n;
+    unsigned char t=(int)(f*255);
+
+
+
+    switch( n){
+        case 0: {
+            c[0] =0;
+            c[1] = t;
+            c[2] = 255;
+            break;
+        };
+        case 1: {
+            c[0] = 0;
+            c[1] = 255-t;
+            c[2] = 255;
+            break;
+        };
+        case 2: {
+            c[0] = t;
+            c[1] = 255;
+            c[2] = 0;
+            break;
+        };
+        case 3: {
+            c[0] = 255;
+            c[1] = 255 - t;
+            c[2] = 0;
+            break;
+        };
+        case 4: {
+            c[0] = t;
+            c[1] = 0;
+            c[2] = 255;
+            break;
+        };
+        case 5: {
+            c[0] = 255;
+            c[1] = 0;
+            c[2] = 255 - t;
+            break;
+        };
+        default: {
+            c[0] = 255;
+            c[1] = 0;
+            c[2] = 0;
+            break;
+        };
+
+    };
+
+};
